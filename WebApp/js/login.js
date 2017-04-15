73fd806adbd1e0e706b6f6b0e1368dfa73853661
domiNik4 <dominika.fric@akademiasovy.sk>
2017-04-15 22:05:33 +0200

    created login page for web application

---
+++ WebApp/js/login.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	$("#username").on("input",function(){
+		checkInput($(this));				
+	});
+
+	$("#username").on("input",function(){
+		checkInput($(this));				
+	});
+
+	$("#loginBtn").on("click",function(){
+		logIn();
+	});
+
+	$(document).keydown(function(event) {
+        if (event.keyCode == 13) {
+            login();
+        }
+    });
+
+	function checkInput(inputField){
+		var input = inputField.val();
+		var latestC=input.charAt(input.length-1)
+		if(latestC.match(/^[0-9a-zA-Z_\-.@#]$/)==null){
+			inputField.val(inputField.val().slice(0, -1));
+		}
+	}
+
+	function logIn(){
+		$("#errorMsg").hide();
+
+		var username=$("#username").val();
+		var password=$("#password").val();
+
+		if(username.length>2&&password.length>2){
+			$.ajax({
+				url: 'http://localhost:8080/login',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    "username": username,
+                    "password": password
+                }),
+                success:function(data){
+                	console.log(data);
+                	var admin=data.admin;
+                    localStorage.setItem("username", admin.photoUrl);
+                    localStorage.setItem("token", data.token);
+                    //window.location.href = "home.html"; not yet
+                }
+                /*error:function(){ //this will be enabled once server returns error after a bad request
+                	$("#errorMsg").html("Incorrect username or password");
+                	$("errorMsg").show();
+                }*/
+                	
+			});
+		}
+		else{
+			$("#errorMsg").show();
+		}
+	}
+});
