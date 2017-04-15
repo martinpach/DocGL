df990bb386e02f91f3cc415c301d98cb8770fdd7
wdfeww <wdfeww@gmail.com>
2017-04-15 11:07:48 +0200

    created new tables and created view in database

--- database/database.sql
+++ database/database.sql
@@ -1,8 +1,8 @@
-create database DocGL;
+CREATE DATABASE DocGL;
 
-use DocGL;
+USE DocGL;
 
-create table Admins(idadmin INTEGER AUTO_INCREMENT,
+CREATE TABLE Admins(idadmin INTEGER AUTO_INCREMENT,
 					firstname VARCHAR(20),
 					lastname VARCHAR(30),
 					email VARCHAR(50),
@@ -10,5 +10,34 @@ create table Admins(idadmin INTEGER AUTO_INCREMENT,
 					password VARCHAR(30),
 					PRIMARY KEY (idadmin));
 
+CREATE TABLE Users(iduser INTEGER AUTO_INCREMENT,
+					firstname VARCHAR(20),
+					lastname VARCHAR(30),
+					email VARCHAR(50),
+					username VARCHAR(20),
+					password VARCHAR(30),
+					PRIMARY KEY (iduser));
+                    
+CREATE TABLE Doctors(iddoctor INTEGER AUTO_INCREMENT,
+					firstname VARCHAR(20),
+					lastname VARCHAR(30),
+					email VARCHAR(50),
+					username VARCHAR(20),
+					password VARCHAR(30),
+					PRIMARY KEY (iddoctor));
+
+insert into Users(firstname,lastname,email,username,password) 
+values("User", "Main", "user@se.sk", "user1", "secret");
+
+insert into Doctors(firstname,lastname,email,username,password) 
+values("Doctor", "Who", "doctor@who.sk", "who", "doc123");	
+
 insert into Admins(firstname,lastname,email,username,password) 
-values("Rasto", "Button", "rasto@button.sk", "rastobutton", "rastobutton123");					
+values("Rasto", "Button", "rasto@button.sk", "rastobutton", "rastobutton123");
+
+CREATE VIEW logins AS 
+SELECT username, password, 'a' AS role FROM Admins
+UNION ALL
+SELECT username, password, 'u' AS role FROM Users
+UNION ALL
+SELECT username, password, 'd' AS role FROM Doctors;
