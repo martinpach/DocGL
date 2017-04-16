6dbeb16db383359074ad577cac8cefe7cab9baea
wdfeww <wdfeww@gmail.com>
2017-04-16 20:29:04 +0200

    database prepared to hash passwords

--- database/database.sql
+++ database/database.sql
@@ -7,31 +7,40 @@ CREATE TABLE Admins(idadmin INTEGER AUTO_INCREMENT,
 					lastname VARCHAR(30),
 					email VARCHAR(50),
 					username VARCHAR(20),
-					password VARCHAR(30),
+					password VARBINARY(200),
 					passwordChanged CHAR(1) DEFAULT 'F',  
-					PRIMARY KEY (idadmin));
+					PRIMARY KEY (idadmin))
+                    ENGINE = InnoDB
+                    DEFAULT CHARACTER SET = utf8
+                    COLLATE = utf8_general_ci;
 
 CREATE TABLE Users(iduser INTEGER AUTO_INCREMENT,
 					firstname VARCHAR(20),
 					lastname VARCHAR(30),
 					email VARCHAR(50),
 					username VARCHAR(20),
-					password VARCHAR(30),
-					PRIMARY KEY (iduser));
+					password VARBINARY(200),
+					PRIMARY KEY (iduser))
+                    ENGINE = InnoDB
+                    DEFAULT CHARACTER SET = utf8
+                    COLLATE = utf8_general_ci;
                     
 CREATE TABLE Doctors(iddoctor INTEGER AUTO_INCREMENT,
 					firstname VARCHAR(20),
 					lastname VARCHAR(30),
 					email VARCHAR(50),
 					username VARCHAR(20),
-					password VARCHAR(30),
-					PRIMARY KEY (iddoctor));
+					password VARBINARY(200),
+					PRIMARY KEY (iddoctor))
+                    ENGINE = InnoDB
+                    DEFAULT CHARACTER SET = utf8
+                    COLLATE = utf8_general_ci;
 
 insert into Users(firstname,lastname,email,username,password) 
-values("User", "Main", "user@se.sk", "user1", "secret");
+values("User", "Main", "user@se.sk", "user1", AES_ENCRYPT('secret', 'sovy2017'));
 
 insert into Doctors(firstname,lastname,email,username,password) 
-values("Doctor", "Who", "doctor@who.sk", "who", "doc123");	
+values("Doctor", "Who", "doctor@who.sk", "who", AES_ENCRYPT('doc123', 'sovy2017'));	
 
 insert into Admins(firstname,lastname,email,username,password) 
-values("Rasto", "Button", "rasto@button.sk", "rastobutton", "rastobutton123");
+values("Rasto", "Button", "rasto@button.sk", "rastobutton", AES_ENCRYPT('rastobutton123', 'sovy2017'));
