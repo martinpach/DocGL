41102cfa4807cd4263020b26e5c1a4e319e8e045
domiNik4 <dominika.fric@akademiasovy.sk>
2017-04-19 22:33:56 +0200

    added mustache template and a dropdown to homepage

--- WebApp/js/admin-script.js
+++ WebApp/js/admin-script.js
@@ -1,1 +1,21 @@
 /*insert js here*/
+
+$(document).ready(function(){
+
+	var data = {
+		idAdmin: localStorage.getItem("idadmin"),
+        firstName: localStorage.getItem("firstName"),
+        lastName: localStorage.getItem("lastName"),
+        email: localStorage.getItem("email"),
+        userName: localStorage.getItem("userName"),
+        passwordChanged: localStorage.getItem("passwordChanged"),
+        token: localStorage.getItem("token")
+    };
+    console.log(localStorage.getItem("firstName"));
+
+    var usernameTemplate = "{{userName}}";
+    var html = Mustache.to_html(usernameTemplate, data.userName);
+    $("#userName").html(html);//sets null since admin object doesn't contain all data yet
+
+	
+});
