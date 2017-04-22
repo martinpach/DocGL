82b882668b411ec35fd9a6588d0a15f1da9fc15c
Solanid <rastislav.ruscak@gmail.com>
2017-04-23 00:30:54 +0200

    renamed database columns

--- database/database.sql
+++ database/database.sql
@@ -2,45 +2,45 @@ CREATE DATABASE DocGL;
 
 USE DocGL;
 
-CREATE TABLE Admins(idadmin INTEGER AUTO_INCREMENT,
-					firstname VARCHAR(20),
-					lastname VARCHAR(30),
+CREATE TABLE Admins(id INTEGER AUTO_INCREMENT,
+					first_name VARCHAR(20),
+					last_name VARCHAR(30),
 					email VARCHAR(50),
-					username VARCHAR(20),
+					user_name VARCHAR(20),
 					password VARBINARY(200),
-					passwordChanged INTEGER DEFAULT 0,  
-					PRIMARY KEY (idadmin))
+					password_changed INTEGER DEFAULT 0,  
+					PRIMARY KEY (id))
                     ENGINE = InnoDB
                     DEFAULT CHARACTER SET = utf8
                     COLLATE = utf8_general_ci;
 
-CREATE TABLE Users(iduser INTEGER AUTO_INCREMENT,
-					firstname VARCHAR(20),
-					lastname VARCHAR(30),
+CREATE TABLE Users(id INTEGER AUTO_INCREMENT,
+					first_name VARCHAR(20),
+					last_name VARCHAR(30),
 					email VARCHAR(50),
-					username VARCHAR(20),
+					user_name VARCHAR(20),
 					password VARBINARY(200),
-					PRIMARY KEY (iduser))
+					PRIMARY KEY (id))
                     ENGINE = InnoDB
                     DEFAULT CHARACTER SET = utf8
                     COLLATE = utf8_general_ci;
                     
-CREATE TABLE Doctors(iddoctor INTEGER AUTO_INCREMENT,
-					firstname VARCHAR(20),
-					lastname VARCHAR(30),
+CREATE TABLE Doctors(id INTEGER AUTO_INCREMENT,
+					first_name VARCHAR(20),
+					last_name VARCHAR(30),
 					email VARCHAR(50),
-					username VARCHAR(20),
+					user_name VARCHAR(20),
 					password VARBINARY(200),
-					PRIMARY KEY (iddoctor))
+					PRIMARY KEY (id))
                     ENGINE = InnoDB
                     DEFAULT CHARACTER SET = utf8
                     COLLATE = utf8_general_ci;
 
-insert into Users(firstname,lastname,email,username,password) 
+insert into Users(first_name,last_name,email,user_name,password) 
 values("User", "Main", "user@se.sk", "user1", AES_ENCRYPT('secret', 'sovy2017'));
 
-insert into Doctors(firstname,lastname,email,username,password) 
+insert into Doctors(first_name,last_name,email,user_name,password) 
 values("Doctor", "Who", "doctor@who.sk", "who", AES_ENCRYPT('doc123', 'sovy2017'));	
 
-insert into Admins(firstname,lastname,email,username,password) 
+insert into Admins(first_name,last_name,email,user_name,password) 
 values("Rasto", "Button", "rasto@button.sk", "rastobutton", AES_ENCRYPT('rastobutton123', 'sovy2017'));
