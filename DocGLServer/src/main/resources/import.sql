baea30ebf132c6050c1d1c79bf63c6ab083dd884
domiNik4 <dominika.fric@akademiasovy.sk>
2017-04-24 16:53:56 +0200

    table column names are the same as in the annotations now

--- DocGLServer/src/main/resources/import.sql
+++ DocGLServer/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 
-insert into admins(email, first_name, last_name, password, user_name) values("rasto@button.sk", "rasto", "button", "rastobutton123", "rastobutton");
-insert into doctors(email, first_name, last_name, password, user_name) values("doctor@who.sk", "doctor", "who", "docwho123", "doctorwho");
-insert into patients(email, first_name, last_name, password, user_name) values("patient@who.sk", "patient", "who", "patwho123", "patientwho");
+insert into Admins(email, first_name, last_name, password, user_name) values("rasto@button.sk", "rasto", "button", "rastobutton123", "rastobutton");
+insert into Doctors(email, first_name, last_name, password, user_name) values("doctor@who.sk", "doctor", "who", "docwho123", "doctorwho");
+insert into Patients(email, first_name, last_name, password, user_name) values("patient@who.sk", "patient", "who", "patwho123", "patientwho");
