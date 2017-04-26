82e8c9e546b9bcd200cb88b2ffe3c50d333f3c2f
martin <martin.pach@akademiasovy.sk>
2017-04-26 14:04:42 +0200

    Added sorting by name into /doctors resource by using query parameters

--- DocGLServer/src/main/java/com/docgl/db/DoctorDAO.java
+++ DocGLServer/src/main/java/com/docgl/db/DoctorDAO.java
@@ -1,12 +1,15 @@
 package com.docgl.db;
 
+import com.docgl.ValidationException;
 import com.docgl.entities.Doctor;
 import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Rasťo on 15.4.2017.
@@ -16,11 +19,24 @@ public class DoctorDAO extends AbstractDAO<Doctor> {
         super(factory);
     }
 
-    public List<Doctor> getAllDoctors(int limit, int start){
+    public List<Doctor> getAllDoctors(int limit, int start, String sortBy, String way){
         if(limit > 0 && start >= 0){
             Criteria criteria = criteria()
                     .setFirstResult(start)
                     .setMaxResults(limit);
+            if(!sortBy.equals("lastName")){
+                throw new ValidationException("Query param '"+ sortBy +"' is not valid");
+            }
+            if(way.equals("asc")){
+                criteria.addOrder(Order.asc(sortBy));
+            }
+            else if(way.equals("desc")){
+                criteria.addOrder(Order.desc(sortBy));
+            }
+            else{
+                throw new ValidationException("Query param '"+ way +"' is not valid. Use instead : 'asc' or 'desc'");
+            }
+
             return list(criteria);
         }
         return list(namedQuery("getAllDoctors"));
