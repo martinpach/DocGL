1d7471d4a36b4da2a2544a57796e907f382315d1
wdfeww <wdfeww@gmail.com>
2017-04-29 16:58:29 +0200

    created CryptorException

--- DocGLServer/src/main/java/com/docgl/Cryptor.java
+++ DocGLServer/src/main/java/com/docgl/Cryptor.java
@@ -1,73 +1,53 @@
 package com.docgl;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.DecoderException;
+import com.docgl.exceptions.CryptorException;
 import org.apache.commons.codec.binary.Hex;
 
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public class Cryptor  {
-    private byte[] getKey(){
-        final byte[] finalKey = new byte[16];
+
+
+    private static final String PASSWORD = "sovy2017";
+    private static byte[] FINAL_KEY = new byte[16];
+
+    static {
         int i = 0;
         try {
-            for(byte b : "sovy2017".getBytes("UTF-8"))
-                finalKey[i++%16] ^= b;
+            for(byte b : PASSWORD.getBytes("UTF-8"))
+                FINAL_KEY[i++%16] ^= b;
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }
-        return finalKey;
-    }
-    public String encrypt(String text){
+        }}
+
+    public static String encrypt(String text) {
         String encriptedtText = null;
         final Cipher encryptCipher;
         try {
             encryptCipher = Cipher.getInstance("AES");
-            encryptCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(getKey(), "AES"));
+            encryptCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(FINAL_KEY, "AES"));
             encriptedtText = new String(Hex.encodeHex(encryptCipher.doFinal(text.getBytes("UTF-8"))));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new CryptorException("An error during encrypting!", e);
         }
         return encriptedtText;
     }
 
-    public String decrypt(String text){
+    public static String decrypt(String text)  {
         String decriptedText = null;
         final Cipher decryptCipher;
         try {
             decryptCipher = Cipher.getInstance("AES");
-            decryptCipher.init(Cipher.DECRYPT_MODE,new SecretKeySpec(getKey(), "AES"));
+            decryptCipher.init(Cipher.DECRYPT_MODE,new SecretKeySpec(FINAL_KEY, "AES"));
             decriptedText = new String(decryptCipher.doFinal(Hex.decodeHex(text.toCharArray())));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (DecoderException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new CryptorException("An error during decryptting!", e);
         }
         return decriptedText;
     }
+
+
 }
