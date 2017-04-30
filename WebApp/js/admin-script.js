fe02f9e8fc420ed021b7b43e1b42039f86c3551d
domiNik4 <dominika.fric@akademiasovy.sk>
2017-04-30 10:53:17 +0200

    table columns reworked, pagination added

--- WebApp/js/admin-script.js
+++ WebApp/js/admin-script.js
@@ -10,7 +10,14 @@ $(document).ready(function () {
         , token: localStorage.getItem("token")
     };
     var ajaxData;
+    var start=0;
+    var limit=4;
+    var countUsers;
+    var countDocs;
+    getCountOfDoctors();
+    getCountOfUsers();
 
+    
     //loads name to the page
     var usernameTemplate = "<p>{{firstName}} {{lastName}}</p>";
     var html = Mustache.to_html(usernameTemplate, data);
@@ -37,7 +44,11 @@ $(document).ready(function () {
     $("#home").on("click",function(){
         $(this).addClass("selected");
         $("#users, #doctors").removeClass("selected");
-        $("#container").load('templates/admin_home.html');        
+        $("#container").load('templates/admin_home.html');
+        $("#paginationContainer").hide();
+        getLikes();
+        getAppointmentCount();
+         
     });
 
     $("#home").trigger("click");
@@ -46,56 +57,192 @@ $(document).ready(function () {
         $(this).addClass("selected");
         $("#users, #home").removeClass("selected");
         $("#container").load('templates/admin_doctors.html');  
-        getDoctors();      
+        getDoctors(start,5);
+        setText(countDocs);
+        setButtons(countDocs);
+        $("#paginationContainer").show();      
     });
 
     $("#users").on("click",function(){
         $(this).addClass("selected");
         $("#home, #doctors").removeClass("selected");
         $("#container").load('templates/admin_users.html');
-        getUsers();
+        getUsers(start,5);
+        setText(countUsers);
+        setButtons(countUsers); 
+        $("#paginationContainer").show();
     });
 
     //logout
     $("#logout").on("click",function(){
+        var dfd = $.Deferred();
         ajaxRequest('/auth/logout','POST').done(function(){
             localStorage.removeItem("token");
             window.location.href = 'index.html';
+            dfd.resolve();
         });
+        return dfd.promise();
     });
 
-
+    //get doctor list
     function getDoctors (){
-        ajaxRequest("/doctors","GET").done(function(){
+        var dfd= $.Deferred();
+        ajaxRequest("/doctors?limit="+limit+"&start="+start,"GET").done(function(){
             var icon='<i class="fa fa-user-md tableIcon"></i>';
-            generateTable($("#tableDoctors"),icon);
+            generateDoctorTable();
+            dfd.resolve();
         });
+        return dfd.promise();
+    }
+    //count of items for pagination purpose
+    function getCountOfDoctors(){
+        var dfd=$.Deferred();
+        ajaxRequest("/doctors?limit="+limit+"&start="+start,"GET").done(function(){
+            countDocs=ajaxData.length;
+            console.log(countDocs);
+            dfd.resolve();
+        })
+        return dfd.promise();
     }
 
-    function getUsers(){
-        ajaxRequest("/patients","GET").done(function(){
-            var icon='<i class="fa fa-user tableIcon"></i>';
-            generateTable($("#tableUsers"),icon);
-            
+    //get user list
+    function getUsers(start,limit){
+        var dfd= $.Deferred();
+        ajaxRequest("/patients?limit="+limit+"&start="+start,"GET").done(function(){
+            generateUserTable();
+            dfd.resolve();            
         });
+        return dfd.promise();
+    }
+    //count of items for pagination purpose
+    function getCountOfUsers(){
+        var dfd=$.Deferred();
+        ajaxRequest("/patients?limit="+limit+"&start="+start,"GET").done(function(){
+            countUsers=ajaxData.length;
+            dfd.resolve();
+        });
+        return dfd.promise();
+    }
+
+    //pagination
+    function setButtons(count){
+        if(count-start>=5)
+            $("#arrowRight").removeClass("disabledBtn");
+        else
+            $("#arrowRight").addClass("disabledBtn");
+        if(start>=6)
+            $("#arrowLeft").removeClass("disabledBtn");
+        else
+            $("#arrowLeft").addClass("disabledBtn");
+    }
+
+    function setText(count){
+        if(count<0){
+            $("#paginationText").hide();
+        }
+        else{
+            var from=start+1;
+            var to;
+            if(start+4>count)
+                to=count;
+            else
+                to=start+4;
+        }
+
+        var paginationText= "Showing "+from+" - "+to+" from " +count;
+        $("#paginationText").html(paginationText);
 
     }
 
-    function generateTable(table,icon){
+    $("#arrowLeft").on("click",function(){
+        start -=5;
+        if($("#doctors").hasClass("selected"))
+            getDoctors(start,5);
+            setText(countDocs);
+            setButtons(countDocs);
+        if($("#users").hasClass("selected"))
+            getUsers(start,5);
+            setText(countUsers);
+            setButtons(countUsers);
+    });
+
+    $("#arrowRight").on("click", function(){
+        console.log("click");
+        start +=5;
+        if($("#doctors").hasClass("selected"))
+            getDoctors(start,5);
+            setText(countDocs);
+            setButtons(countDocs);
+        if($("#users").hasClass("selected"))
+            getUsers(start,5);
+            setText(countUsers);
+            setButtons(countUsers);
+    });
+
+    //generate table for users/docs
+    function generateDoctorTable(){
         var i=0;
+        var icon='<i class="fa fa-user-md tableIcon"></i>';
         for(i=0;i<ajaxData.length;i++){
-            table.append('<tr>'+
+            $("#tableDoctors").append('<tr>'+
                 '<td>'+icon+'</td>'+
                 '<td>'+ajaxData[i].id+'</td>'+
-                '<td>'+ajaxData[i].firstName+'</td>'+
-                '<td>'+ajaxData[i].lastName+'</td>'+
+                '<td>'+ajaxData[i].firstName+" "+ajaxData[i].lastName+'</td>'+
+                '<td>'+ajaxData[i].specialization+'</td>'+
                 '<td>'+ajaxData[i].email+'</td>'+
-                '</tr>');
-            if(i=0)
-                console.log(ajaxData[i].id);            
+                '<td><p class="like"><i class="fa fa-heart heart"></i>'+ajaxData[i].likes+'</p></td>'+
+                '</tr>');        
         }
     }
+
+    function generateUserTable(){
+        var i=0;
+        var icon='<i class="fa fa-user tableIcon"></i>';
+        for(i=0;i<ajaxData.length;i++){
+            $("#tableUsers").append('<tr>'+
+                '<td>'+icon+'</td>'+
+                '<td>'+ajaxData[i].id+'</td>'+
+                '<td>'+ajaxData[i].firstName+" "+ajaxData[i].lastName+'</td>'+
+                '<td>'+ajaxData[i].email+'</td>'+
+                '<td>'+ajaxData[i].registrationDate+'</td>'+
+                '</tr>');        
+        }
+
+    }
+
+    //get statistics
+    function getAppointmentCount(){
+        var dfd = $.Deferred();
+        ajaxRequest("/appointments/count","GET").done(function(){
+            console.log(ajaxData);
+            dfd.resolve();
+        });
+        return dfd.promise();
+    }
+
+    function getLikes(){
+        var dfd=$.Deferred();
+        ajaxRequest("/doctors/likes","GET").done(function(){
+            console.log(ajaxData);
+            dfd.resolve();
+        });
+        return dfd.promise();
+    }
+
+
+
+    function editUsername(){
+
+    }
     
+    function editPassword(){
+
+    }
+
+    function editEmail(){
+
+    }
+
     //ajax request function
     function ajaxRequest(url, requestType, dataToSend) {
         var dfd = $.Deferred();
@@ -108,7 +255,7 @@ $(document).ready(function () {
             data: dataToSend,
             contentType: 'application/json',
             success: function (data) {
-                /*if (data != null) */ajaxData = data;
+                if (data != null) ajaxData = data;
                 dfd.resolve();
             },
             error: function () {
