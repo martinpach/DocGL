31b50356a14693e240a8a7e4dd00d0f6ab70e309
Solanid <rastislav.ruscak@gmail.com>
2017-05-14 21:25:31 +0200

    updated database imports

--- DocGLServer/src/main/java/com/docgl/db/AppointmentDAO.java
+++ DocGLServer/src/main/java/com/docgl/db/AppointmentDAO.java
@@ -108,6 +108,7 @@ public class AppointmentDAO extends AbstractDAO<Appointment> {
                     currentSession().find(Doctor.class, input.getDoctorId()),
                     currentSession().find(Patient.class, patientId),
                     new Time((long)(input.getTime().getTime() + 3.6e+6)),
+                    //input.getTime(),
                     input.getDate(),
                     input.getFirstName(),
                     input.getLastName()
@@ -143,6 +144,7 @@ public class AppointmentDAO extends AbstractDAO<Appointment> {
                 Appointment appointmentToUpdate = currentSession().find(Appointment.class, appointment.getId());
                 appointmentToUpdate.setCanceled(true);
                 appointmentToUpdate.setTime(new Time((long)(appointment.getTime().getTime() + 3.6e+6)));
+                //appointmentToUpdate.setTime(appointment.getTime());
             }
         }
 
