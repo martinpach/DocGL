b7803ea49aadc0538b3c569144b78c978157d2c0
domiNik4 <dominika.fric@akademiasovy.sk>
2017-05-14 19:32:26 +0200

    doc profile can be edited now

--- WebApp/js/doctor-script.js
+++ WebApp/js/doctor-script.js
@@ -33,12 +33,21 @@ $(document).ready(function() {
     var likesTemplate = "{{likes}}";
     var html = Mustache.to_html(likesTemplate, docData);
     $("#likesCount").html(html);
+    console.log(docData.likes);
 
     $("#myProfile").on("click", function() {
         $("#container").load('templates/doctor_profile.html', function() {
             var template = "<p>{{userName}}'s</p>";
             var html = Mustache.to_html(template, docData);
             $("#heading").html(html);
+            $("#firstname").html(docData.firstName);
+            $("#lastname").html(docData.lastName);
+            $("#spec").html(docData.specialization);
+            $("#phone").html(docData.phone);
+            $("#email").html(docData.email);
+            $("#city").html(docData.city);
+            $("#workplace").html(docData.workplace);
+            $("#password").html("***********");
         });
     });
 
@@ -94,8 +103,7 @@ $(document).ready(function() {
             $("#nextAppointmentTime").html(html);
             }
             else{
-                $("#nextAppointmentAt").html("");
-                $("#nextAppointmentTime").html("No appointments left.");
+                $("#nextAppointmentContainer").hide();
             }
             dfd.resolve();
         });
@@ -128,6 +136,119 @@ $(document).ready(function() {
         return dfd.promise();
 
     }
+
+    $(document).on("click", "#editProfile", function (event) {
+        event.preventDefault(event);
+        $("#firstname, #lastname, #email, #phone").toggle();
+        $("#changeFirstnameDiv,#changeLastnameDiv,#changeEmailDiv,#changePhoneDiv,#submitProfile").toggle();
+        $("#editFirstnameInput").val(docData.firstName);
+        $("#editLastnameInput").val(docData.lastName);
+        $("#editEmailInput").val(docData.email);
+        $("#editPhoneInput").val(docData.phone);
+        $(".profileErrMsg,#profileSuccessMsg").html("");       
+    });
+
+    $(document).on("click", "#submitProfile", function () {
+        var dfd = $.Deferred();
+        var newData = {
+            firstname: $("#editFirstnameInput").val(),
+            lastname: $("#editLastnameInput").val(),
+            email: $("#editEmailInput").val(),
+            phone: $("#editPhoneInput").val(),
+            password: ""
+        }
+        var regex = {
+            name: /^[a-zA-Z\-]+$/,
+            email:/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/,
+            phone:/^[0-9]{10}|([0-9]{4}\s[0-9]{3}\s[0-9]{3})|\+[0-9]{12}|\+[0-9]{3}\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/
+        };
+        var isFirstnameValid = false;
+        var isLastnameValid=false;
+        var isEmailValid = false;
+        var isPhoneValid=false;
+
+        if (regex.name.test(newData.firstname))
+            isFirstnameValid = true;
+        else
+            $("#firstnameErrorMsg").html("Invalid first name.");
+
+        if (regex.name.test(newData.lastname))
+            isLastnameValid = true;
+        else
+             $("#lastnameErrorMsg").html("Invalid last name.");
+
+        if (regex.email.test(newData.email)) 
+            isEmailValid = true;
+        else
+             $("#emailErrorMsg").html("Invalid email format.");
+         if(regex.phone.test(newData.phone))
+            isPhoneValid=true;
+
+        if (isFirstnameValid&&isLastnameValid && isEmailValid&&isPhoneValid) {
+            var dataToSend = JSON.stringify({
+                "firstName": newData.firstname,
+                "lastName": newData.lastname,
+                "password": newData.password,
+                "email": newData.email,
+                "phone":newData.phone
+            });
+            ajaxRequest("/doctors/" + docData.id + "/profile", "PUT", dataToSend).done(function () {
+                $("#emailErrorMsg").html("Profile changed successfully.");
+                localStorage.setItem("firstName", ajaxData.firstName);
+                localStorage.setItem("lastName", ajaxData.lastName);
+                localStorage.setItem("email", ajaxData.email);
+                localStorage.setItem("phone", ajaxData.phone);
+                docData.firstName = ajaxData.firstName;
+                docData.lastName = ajaxData.lastName;
+                docData.email = ajaxData.email;
+                docData.phone=ajaxData.phone;
+                $("#editFirstnameInput,#firstname").val(ajaxData.firstName);
+                $("#editLastnameInput,#lastname").val(ajaxData.lastName);
+                $("#editEmailInput,#email").val(ajaxData.email);
+                $("#editPhoneInput,#phone").val(ajaxData.phone);
+                var template = "<p>{{firstName}} {{lastName}}</p>";
+                html = Mustache.to_html(template, ajaxData);
+                $("#userName").html(html);
+                $(".profileErrMsg").html("");
+                $("#profileSuccessMsg").html("Profile changed successfully");
+        
+                dfd.resolve();
+
+            });
+            return dfd.promise();
+        }
+    });
+
+    $(document).on("click","#editPassword",function(){
+        $("#password,#changePasswordDiv,#submitPassword").toggle();
+        $("#pwdErrorMsg").html("");
+    });
+
+    $(document).on("click","#submitPassword",function(){
+        var dfd=$.Deferred();
+        var newPassword={
+            p1:$("#editPwd1").val(),
+            p2:$("#editPwd2").val()
+        }
+        var pwdRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\/\^&\*])/;
+        var isPasswordValid=false;
+
+        if (pwdRegex.test(newPassword.p1) && newPassword.p1 == newPassword.p2)
+            isPasswordValid=true;
+
+        if(isPasswordValid==false)
+            $("#pwdErrorMsg").html("Invalid Password!");
+        else{
+            var dataToSend=JSON.stringify({
+                "password":newPassword.p1
+            });
+            ajaxRequest("/doctors/" + docData.id + "/profile/password","PUT",dataToSend).done(function(){
+                $("#pwdErrorMsg").html("password changed successfully!");
+                dfd.resolve();
+            });
+            return dfd.promise();
+        }
+    });
     
 
 
