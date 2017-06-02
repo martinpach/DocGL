83333d469dd3f399b1c9226ce86d64ec5411f7c5
wdfeww <wdfeww@gmail.com>
2017-06-02 10:06:47 +0200

    android - now updating device token after register

--- android/app/src/main/java/com/wdfeww/docgl/Register.java
+++ android/app/src/main/java/com/wdfeww/docgl/Register.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.gson.Gson;
 import com.wdfeww.docgl.data.methods.Checker;
 import com.wdfeww.docgl.data.methods.JsonReqestBody;
@@ -20,6 +22,7 @@ import com.wdfeww.docgl.data.remote.ServiceGenerator;
 import org.json.JSONObject;
 
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -104,6 +107,7 @@ public class Register extends AppCompatActivity {
                     prefsEditor.putString("LoggedPatient", json);
                     prefsEditor.commit();
                     redirectToHome();
+                    updateFCMToken();
                     sign_up.setEnabled(false);
                 }
             }
@@ -120,6 +124,33 @@ public class Register extends AppCompatActivity {
         Intent intent = new Intent(getBaseContext(), Home.class);
         this.finish();
         startActivity(intent);
+    }
+
+    private void updateFCMToken(){
+        JSONObject json = JsonReqestBody.updateFCMRegistrationToken(FirebaseInstanceId.getInstance().getToken());
+        RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), (json.toString()));
+        Service loginService =
+                ServiceGenerator.createService(Service.class, user.getToken());
+        Call<ResponseBody> call = loginService.updatePatientsFCMRegistrationToken(user.getPatient().getId(), body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.d("DocGL", "Token: token was updated" );
+
+
+                } else {
+                    Log.d("DocGL", "Token: token was not updated" );
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.d("DocGL", "Token: Server error "+ t.getMessage() );
+
+            }
+        });
     }
 
 }
